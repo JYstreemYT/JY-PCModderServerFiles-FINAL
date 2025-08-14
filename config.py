46871d0a42696436b5c04abe4be529c26c3aab3f
J͓̽Y͓̽s͓̽t͓̽r͓̽e͓̽e͓̽m͓̽Y͓̽T͓̽ <nintendodslite2023@gmail.com>
2025-08-14 18:08:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # place any of your own overrides here. 
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-# config['partyName'] = 'My Awesome Party'
+# config['partyName'] = 'THE OG VS NEW || EPIC TEAMS'
 # config['sessionType'] = 'teams'
 # config['maxPartySize'] = 6
 # config['port'] = 43209
